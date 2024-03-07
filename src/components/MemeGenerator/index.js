c52e7856a608646ff184d01c6d72246ee7c2ef32
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-07 14:14:40 +0000

    index file coded

--- src/components/MemeGenerator/index.js
+++ src/components/MemeGenerator/index.js
@@ -1,3 +1,5 @@
+import {Component} from 'react'
+
 const fontSizesOptionsList = [
   {
     optionId: '8',
@@ -29,3 +31,36 @@ const fontSizesOptionsList = [
   },
 ]
 // Write your code here
+
+class MemeGenerator extends Component {
+  state = {
+    imageUrl: '',
+    topText: '',
+    bottomText: '',
+    fontSize: parseInt(fontSizesOptionsList[0].displayText),
+  }
+
+  onChangeTopText = event => {
+    this.setState({topText: event.target.value})
+  }
+
+  onChangeBottomText = event => {
+    this.setState({bottomText: event.target.value})
+  }
+
+  onChangeImageUrl = event => {
+    this.setState({imageUrl: event.target.value})
+  }
+
+  onChangeOption = event => {
+    this.setState({fontSize: event.target.value})
+  }
+
+  render() {
+    const {imageUrl, topText, bottomText, fontSize} = this.state
+
+    return <p>{(imageUrl, topText, bottomText, fontSize)}</p>
+  }
+}
+
+export default MemeGenerator
